a74a767b1c673a80acfd6f8cdcee4d6f2e42fc78
George Danezis <g.danezis@ucl.ac.uk>
2015-02-12 23:55:52 +0000

    Added Lab 03 code files

---
+++ Lab03Compute/Lab03Code.py
@@ -0,0 +1,200 @@
+#####################################################
+# GA17 Privacy Enhancing Technologies -- Lab 03
+#
+# Basics of Privacy Friendly Computations through
+#         Additive Homomorphic Encryption.
+#
+# Run the tests through:
+# $ py.test -v test_file_name.py
+
+#####################################################
+# TASK 1 -- Setup, key derivation, log
+#           Encryption and Decryption
+#
+
+from petlib.ec import EcGroup
+
+def setup():
+    """Generates the Cryptosystem Parameters."""
+    G = EcGroup(nid=713)
+    g = G.hash_to_point(b"g")
+    h = G.hash_to_point(b"h")
+    o = G.order()
+    return (G, g, h, o)
+
+def keyGen(params):
+   """ Generate a private / public key pair """
+   (G, g, h, o) = params
+   
+   # ADD CODE HERE
+
+   return (priv, pub)
+
+def encrypt(params, pub, m):
+    """ Encrypt a message under the public key """
+    if not -100 < m < 100:
+        raise Exception("Message value to low or high.")
+
+   # ADD CODE HERE
+   # Encrypt as (k*g, k*pub + m*h)
+
+    return c
+
+def isCiphertext(params, ciphertext):
+    """ Check a ciphertext """
+    (G, g, h, o) = params
+    ret = len(ciphertext) == 2
+    a, b = ciphertext
+    ret &= G.check_point(a)
+    ret &= G.check_point(b)
+    return ret
+
+_logh = None
+def logh(params, hm):
+    """ Compute a discrete log, for small number only """
+    global _logh
+    (G, g, h, o) = params
+
+    # Initialize the map of logh
+    if _logh == None:
+        _logh = {}
+        for m in range (-1000, 1000):
+            _logh[(m * h)] = m
+
+    if hm not in _logh:
+        raise Exception("No decryption found.")
+
+    return _logh[hm]
+
+def decrypt(params, priv, ciphertext):
+    """ Decrypt a message using the private key """
+    assert isCiphertext(params, ciphertext)
+    a , b = ciphertext
+
+   # ADD CODE HERE
+
+    return logh(params, hm)
+
+#####################################################
+# TASK 2 -- Define homomorphic addition and
+#           multiplication with a public value
+# 
+
+def add(params, pub, c1, c2):
+    """ Given two ciphertexts compute the ciphertext of the 
+        sum of their plaintexts.
+    """
+    assert isCiphertext(params, c1)
+    assert isCiphertext(params, c2)
+
+   # ADD CODE HERE
+
+    return c3
+
+def mul(params, pub, c1, alpha):
+    """ Given a ciphertext compute the ciphertext of the 
+        product of the plaintext time alpha """
+    assert isCiphertext(params, c1)
+
+   # ADD CODE HERE
+
+    return c3
+
+#####################################################
+# TASK 3 -- Define Group key derivation & Threshold
+#           decryption. Assume an honest but curious 
+#           set of authorities.
+
+def groupKey(params, pubKeys=[]):
+    """ Generate a group public key from a list of public keys """
+    (G, g, h, o) = params
+
+   # ADD CODE HERE
+
+    return pub
+
+def partialDecrypt(params, priv, ciphertext, final=False):
+    """ Given a ciphertext and a private key, perform partial decryption. 
+        If final is True, then return the plaintext. """
+    assert isCiphertext(params, ciphertext)
+    
+    # ADD CODE HERE
+
+    if final:
+        return logh(params, b1)
+    else:
+        return a1, b1
+
+#####################################################
+# TASK 4 -- Actively corrupt final authority, derives
+#           a public key with a known private key.
+#
+
+def corruptPubKey(params, priv, OtherPubKeys=[]):
+    """ Simulate the operation of a corrupt decryption authority. 
+        Given a set of public keys from other authorities return a
+        public key for the corrupt authority that leads to a group
+        public key corresponding to a private key known to the
+        corrupt authority. """
+    (G, g, h, o) = params
+    
+   # ADD CODE HERE
+
+    return pub
+
+#####################################################
+# TASK 5 -- Implement operations to support a simple
+#           private poll.
+#
+
+def encode_vote(params, pub, vote):
+    """ Given a vote 0 or 1 encode the vote as two
+        ciphertexts representing the count of votes for
+        zero and the votes for one."""
+    assert vote in [0, 1]
+
+   # ADD CODE HERE
+
+    return (v0, v1)
+
+def process_votes(params, pub, encrypted_votes):
+    """ Given a list of encrypted votes tally them
+        to sum votes for zeros and votes for ones. """
+    assert isinstance(encrypted_votes, list)
+    
+   # ADD CODE HERE
+
+    return tv0, tv1
+
+def simulate_poll(votes):
+    """ Simulates the full process of encrypting votes,
+        tallying them, and then decrypting the total. """
+        
+    # Generate parameters for the crypto-system
+    params = setup()
+
+    # Make keys for 3 authorities
+    priv1, pub1 = keyGen(params)
+    priv2, pub2 = keyGen(params)
+    priv3, pub3 = keyGen(params)
+    pub = groupKey(params, [pub1, pub2, pub3])
+
+    # Simulate encrypting votes
+    encrypted_votes = []
+    for v in votes:
+        encrypted_votes.append(encode_vote(params, pub, v))
+
+    # Tally the votes
+    total_v0, total_v1 = process_votes(params, pub, encrypted_votes)
+
+    # Simulate threshold decryption
+    privs = [priv1, priv2, priv3]
+    for priv in privs[:-1]:
+        total_v0 = partialDecrypt(params, priv, total_v0)
+        total_v1 = partialDecrypt(params, priv, total_v1)
+
+    total_v0 = partialDecrypt(params, privs[-1], total_v0, True)
+    total_v1 = partialDecrypt(params, privs[-1], total_v1, True)
+
+    # Return the plaintext values
+    return total_v0, total_v1
---
+++ Lab03Compute/Lab03Tests.py
@@ -0,0 +1,144 @@
+#####################################################
+# GA17 Privacy Enhancing Technologies -- Lab 03
+#
+# Basics of Privacy Friendly Computations through
+#         Additive Homomorphic Encryption.
+#
+# Run the tests through:
+# $ py.test -v test_file_name.py
+
+#####################################################
+# TASK 1 -- Setup, key derivation, log
+#           Encryption and Decryption
+
+import pytest
+from pytest import raises
+
+from Lab03Code import *
+
+@pytest.mark.task1
+def test_encrypt():
+    params = setup()
+    priv, pub = keyGen(params)
+    assert encrypt(params, pub, 0)
+    assert encrypt(params, pub, 10)
+    assert encrypt(params, pub, -10)
+    with raises(Exception) as excinfo:
+        encrypt(params, pub, -1000)
+    with raises(Exception) as excinfo:
+        encrypt(params, pub, 1000)
+
+@pytest.mark.task1
+def test_decrypt():
+    params = setup()
+    priv, pub = keyGen(params)
+    assert decrypt(params, priv, encrypt(params, pub, 0)) == 0
+    assert decrypt(params, priv, encrypt(params, pub, 2)) == 2
+    assert decrypt(params, priv, encrypt(params, pub, -2)) == -2
+    assert decrypt(params, priv, encrypt(params, pub, 99)) == 99
+
+#####################################################
+# TASK 2 -- Define homomorphic addition and
+#           multiplication with a public value
+
+@pytest.mark.task2
+def test_add():
+    params = setup()
+    priv, pub = keyGen(params)
+    one = encrypt(params, pub, 1)
+    two = encrypt(params, pub, 2)
+    three = add(params, pub, one, two)
+    assert decrypt(params, priv, three) == 3
+
+    # Try it for a range of numbers
+    for x in range(-10, 10):
+        Ex = encrypt(params, pub, x)
+        E2x = add(params, pub, Ex, Ex)
+        assert decrypt(params, priv, E2x) == 2*x
+
+@pytest.mark.task2
+def test_mul():
+    params = setup()
+    priv, pub = keyGen(params)
+    two = encrypt(params, pub, 2)
+    three = mul(params, pub, two, 2)
+    assert decrypt(params, priv, three) == 4
+
+    # Try it for a range of numbers
+    for x in range(-10, 10):
+        Ex = encrypt(params, pub, x)
+        E2x = mul(params, pub, Ex, 20)
+        assert decrypt(params, priv, E2x) == 20*x
+
+#####################################################
+# TASK 3 -- Define Group key derivation & Threshold
+#           decryption.
+
+@pytest.mark.task3
+def test_groupKey():
+    params = setup()
+    (G, g, h, o) = params
+
+    # Generate a group key
+    priv1, pub1 = keyGen(params)
+    priv2, pub2 = keyGen(params)
+    pub = groupKey(params, [pub1, pub2])
+
+    # Check it is valid
+    priv = (priv1 + priv2) % o
+    assert decrypt(params, priv, encrypt(params, pub, 0)) == 0
+
+@pytest.mark.task3
+def test_partial():
+    params = setup()
+    (G, g, h, o) = params
+
+    # Generate a group key
+    priv1, pub1 = keyGen(params)
+    priv2, pub2 = keyGen(params)
+    pub = groupKey(params, [pub1, pub2])
+
+    # Each authority decrypts in turn
+    c = encrypt(params, pub, 0)
+    cprime = partialDecrypt(params, priv1, c)
+    m = partialDecrypt(params, priv2, cprime, True)
+    assert m == 0
+
+#####################################################
+# TASK 4 -- Actively corrupt final authority, derives
+#           a public key with a known private key.
+#
+
+@pytest.mark.task4
+def test_badpub():
+    params = setup()
+    (G, g, h, o) = params
+
+    # Four authorities generate keys
+    priv1, pub1 = keyGen(params)
+    priv2, pub2 = keyGen(params)
+    priv3, pub3 = keyGen(params)
+    priv4, pub4 = keyGen(params)
+
+    # Derive a bad key
+    x = o.random()
+    badpub = corruptPubKey(params, x, [pub1, pub2, pub3, pub4])
+
+    # Derive the group key including the bad public key
+    pub = groupKey(params, [pub1, pub2, pub3, pub4, badpub])
+
+    # Check that the corrupt authority can decrypt a message
+    # encrypted under the group key with its secret only.
+    assert decrypt(params, x, encrypt(params, pub, 0)) == 0
+
+#####################################################
+# TASK 5 -- Implement operations to support a simple
+#           private poll.
+#
+
+@pytest.mark.task5
+def test_poll():
+    votes = [1, 0, 1, 0, 1, 1, 0, 1, 1, 1]
+    v0, v1 = simulate_poll(votes)
+    assert v0 == 3
+    assert v1 == 7
